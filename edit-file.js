10085a059ccf0ba5306698417aa6483d6bca290e
Aileen Murphy <ai.murf2@gmail.com>
2019-07-12 20:59:48 -0700

    testing

--- edit-file.js
+++ edit-file.js
@@ -2,30 +2,54 @@
 
 const fs = require ('fs');
 const faker = require ('faker');
-const kingleer = process.argv.slice(2);
-const filepath = 'files/'+kingleer;
 
-console.log('kingleer is', kingleer);
-console.log('file is', filepath);
+const file = process.argv.slice(2);
+// const filepath = file.map((v)=> 'files/'+v).toString();
+// 'files/'+file;
+console.log('file is', file);
+// console.log('file is', filepath);
+
+let testData = faker.name.findName();
+let bufferData = Buffer.from(testData.toString());
+
+//__dirname is the path that is running
+fs.readFile(`${__dirname}/files/${file}`, (err,data)=>{
+  if(err)throw err;
+  let fileContent = data.toString().trim();
 
 
-let fakerData = faker.name.findName();
+  fs.writeFile(`${__dirname}/files/${file}`,bufferData,(err,data)=>{
+    if(err)throw err;
 
-//fs.writeFile(file, data[, options], callback)
-let write = function(){fs.writeFile(filepath, fakerData, (err, data) => {
-  if (err) throw err;
-  console.log('The file has been saved!');
-});
-}
+    fs.readFile(`${__dirname}/files/${file}`, (err,data)=>{
+      if(err)throw err;
+
+      let fileContent = data.toString().trim();
+
+      console.log('file content is',fileContent)
+    })
+  })
+})
 
 
-//fs.readFile(file[, options], callback)
-let read = function(){fs.readFile(filepath, (err, data) => {
-  if (err) throw err;
-  console.log(data.toString());
-});
-}
 
-read();
-write();
-read();
+
+
+// //fs.writeFile(file, data[, options], callback)
+// let write = function(){fs.writeFile(filepath, testData, (err, data) => {
+//   if (err) throw err;
+//   console.log('The file has been saved!');
+// });
+// }
+
+
+// //fs.readFile(file[, options], callback)
+// let read = function(){fs.readFile(filepath, (err, data) => {
+//   if (err) throw err;
+//   console.log(data.toString());
+// });
+// }
+
+// read();
+// write();
+// read();
